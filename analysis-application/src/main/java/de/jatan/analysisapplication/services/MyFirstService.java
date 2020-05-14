d3898b2d4994df8e816e41be82474fc603654d89
Jan Vogt <jan_vogt92@web.de>
2020-05-14 11:51:25 +0200

    setup/feat: add docker-compose file for local db setup and create CRUD acces with restcontroller and hibernate

--- analysis-application/src/main/java/de/jatan/analysisapplication/services/MyFirstService.java
+++ analysis-application/src/main/java/de/jatan/analysisapplication/services/MyFirstService.java
@@ -5,7 +5,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import de.jatan.analysisapplication.entities.AppVisitorList;
+import de.jatan.analysisapplication.Database.entities.AppVisitorList;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,36 +13,37 @@ import javax.persistence.PersistenceContext;
 @Service
 public class MyFirstService {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+  @PersistenceContext
+  private EntityManager entityManager;
 
-    public MyFirstService() {
-        System.out.println("MyFirstService is created");
+  public MyFirstService() {
+    System.out.println("MyFirstService is created");
+  }
+
+  @EventListener(classes = ApplicationReadyEvent.class)
+  public void init() {
+    System.out.println("MyFirstService is Ready and Running");
+  }
+
+  static int counter = 0;
+
+  @Transactional
+  public HelloDto hello(String user) {
+    counter++;
+    String visitors = getVisitorList(user);
+    return new HelloDto(String.format(
+        "Hello Jatan Member %s, \n this application has been called %s times. \n Following User visited the page %s",
+        user, counter, visitors));
+  }
+
+  private String getVisitorList(String user) {
+    AppVisitorList result = entityManager.find(AppVisitorList.class, 1L);
+    if (result == null) {
+      AppVisitorList newList = new AppVisitorList();
+      result = newList;
+      entityManager.persist(newList);
     }
-
-    @EventListener(classes = ApplicationReadyEvent.class)
-    public void init() {
-        System.out.println("MyFirstService is Ready and Running");
-    }
-
-
-    static int counter = 0;
-
-    @Transactional
-    public HelloDto hello(String user) {
-        counter++;
-        String visitors = getVisitorList(user);
-        return new HelloDto(String.format("Hello Jatan Member %s, \n this application has been called %s times. \n Following User visited the page %s", user, counter, visitors));
-    }
-
-    private String getVisitorList(String user) {
-        AppVisitorList result = entityManager.find(AppVisitorList.class, 1L);
-        if (result == null) {
-            AppVisitorList newList = new AppVisitorList();
-            result = newList;
-            entityManager.persist(newList);
-        }
-        result.addVisitor(user);
-        return result.getListAsString();
-    }
+    result.addVisitor(user);
+    return result.getListAsString();
+  }
 }
