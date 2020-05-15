0e68036066a2ba4065a4fa6d54cc2135a438eec4
Jan Vogt <jan_vogt92@web.de>
2020-05-15 16:04:06 +0200

    chore: clean unused imports

--- analysis-application/src/main/java/de/jatan/analysisapplication/controller/GithubController.java
+++ analysis-application/src/main/java/de/jatan/analysisapplication/controller/GithubController.java
@@ -2,11 +2,7 @@ package de.jatan.analysisapplication.controller;
 
 import java.util.List;
 import java.util.stream.Stream;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
