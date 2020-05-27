e891349d45f3e41955e20f319e4bac9c8849eee8
Jan Vogt <jan_vogt92@web.de>
2020-05-27 14:55:48 +0200

    feat: add sonarqube response handling with objects by webhook endpoint

--- analysis-application/src/main/java/de/jatan/analysisapplication/controller/SonarQubeController.java
+++ analysis-application/src/main/java/de/jatan/analysisapplication/controller/SonarQubeController.java
@@ -1,17 +1,25 @@
-/*
- * package de.jatan.analysisapplication.controller;
- *
- * import org.springframework.web.bind.annotation.GetMapping; import
- * org.springframework.web.bind.annotation.RequestBody; import
- * org.springframework.web.bind.annotation.RequestMapping; import
- * org.springframework.web.bind.annotation.RequestParam;
- *
- * public class SonarQubeController {
- *
- * @RequestMapping(path = "/sonar")
- *
- * @GetMapping(path = "/hook") public void getGithubUser(@RequestBody
- * SonarStrucre sonarbody) { }
- *
- * }
- */
+
+package de.jatan.analysisapplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.jatan.analysisapplication.Domain.Model.SonarQubeResponse;
+
+@RestController
+@RequestMapping(path = "/sonarqube")
+public class SonarQubeController {
+
+  @PostMapping(path = "/hook")
+  public void printSonarQubeStack(@RequestBody SonarQubeResponse sonarbody) {
+    System.err.println(sonarbody.getProject().getKey());
+  }
+
+  @GetMapping(path = "/create")
+  public String getHealth() {
+    return "erstelle neues sonar-project";
+  }
+}
