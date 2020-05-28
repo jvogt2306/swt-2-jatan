13332c90e56cae6a58ed810ff037bbf3a6735c6d
Jan Vogt <jan_vogt92@web.de>
2020-05-28 10:41:17 +0200

    feat: create sonar scan with process builder with static project name in directory

--- analysis-application/src/main/java/de/jatan/analysisapplication/services/SonarQubeService.java
+++ analysis-application/src/main/java/de/jatan/analysisapplication/services/SonarQubeService.java
@@ -7,36 +7,56 @@ import org.springframework.stereotype.Service;
 @Service
 public class SonarQubeService {
 
+  private String applicationPath = System.getProperty("user.dir");
+  private String repositoryPath = "/src/main/resources/Repositories/";
+
   public void scanRepository() {
-    // String cmdSonarScanner = "sonar-scanner";
-    String applicationPath = System.getProperty("user.dir");
-    String repositoryPath = "/src/main/resources/Repositories/";
-    String projectName = "projekt1-ar-react-test";
-    String projectPath = applicationPath + repositoryPath + projectName;
-    // String projectPath = applicationPath;
-    System.err.println(projectPath);
-
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    processBuilder.command("bash", "cd", projectPath);
-    processBuilder.command("sonar-scanner", "-Dsonar.sources=src -X");
-    // processBuilder.command("pwd");
-    // processBuilder.command("bash", cmdSonarScanner);
     try {
-      Process process = processBuilder.start();
-      StringBuilder output = new StringBuilder();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      String line;
-
-      while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-      process.waitFor();
-      System.err.println(output);
+      String projectName = "projekt1-ar-react-test";
+      String repositoryFolder = applicationPath + repositoryPath;
+      String projectPath = repositoryFolder + projectName;
+      createSonarPropertiesFile(repositoryFolder, projectName);
+      doScanRespository(projectPath);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
+  }
 
+  private void doScanRespository(String path) throws InterruptedException, IOException {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    processBuilder.command("bash", "cd", path);
+    processBuilder.command("sonar-scanner");
+    executeProcesses(processBuilder);
+  }
+
+  private void createSonarPropertiesFile(String path, String projectName) throws IOException {
+    File sonarPropertiesFile = new File("sonar-project.properties");
+    BufferedWriter writer = new BufferedWriter(new FileWriter(sonarPropertiesFile, true));
+    writeSonarProperties(writer, projectName);
+  }
+
+  private void writeSonarProperties(BufferedWriter writer, String projectName) throws IOException {
+    writer.write("sonar.sources=src");
+    writer.newLine();
+    // writer.write("sonar.java.binaries=bin");
+    // writer.newLine();
+    writer.write("sonar.projectKey=" + projectName);
+    writer.close();
+  }
+
+  private void executeProcesses(ProcessBuilder processBuilder) throws InterruptedException, IOException {
+    Process process = processBuilder.start();
+    StringBuilder output = new StringBuilder();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      output.append(line + "\n");
+    }
+    process.waitFor();
+    System.err.println(output);
   }
 }
