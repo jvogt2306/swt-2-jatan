8b30079452a8f7bb25b2c6ce34e8259ea63cbc9b
Jan Vogt <jan_vogt92@web.de>
2020-05-28 15:25:51 +0200

    feat: add dotenv for github credentials - and example .env structure

--- analysis-application/src/main/java/de/jatan/analysisapplication/services/GithubService.java
+++ analysis-application/src/main/java/de/jatan/analysisapplication/services/GithubService.java
@@ -8,11 +8,13 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import de.jatan.analysisapplication.Domain.Model.GithubRepository;
 import de.jatan.analysisapplication.Domain.Model.GithubUser;
 import de.jatan.analysisapplication.controller.DTO.RepositoryDTO;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Service
 public class GithubService {
@@ -32,9 +34,13 @@ public class GithubService {
   }
 
   public String cloneRepository(RepositoryDTO dto) throws InvalidRemoteException, TransportException, GitAPIException {
+    Dotenv dotenv = Dotenv.load();
+    String githubUsername = dotenv.get("githubUsername");
+    String githubPassword = dotenv.get("githubPassword");
     String applicationPath = System.getProperty("user.dir");
     Git.cloneRepository().setURI(dto.url)
-        .setDirectory(new File(applicationPath + "/src/main/resources/repositories/" + dto.projectName)).call();
-    return "suchen ..." + dto.url;
+        .setDirectory(new File(applicationPath + "/src/main/resources/repositories/" + dto.projectName))
+        .setCredentialsProvider(new UsernamePasswordCredentialsProvider(githubUsername, githubPassword)).call();
+    return "suchen nach ... " + dto.url;
   }
 }
