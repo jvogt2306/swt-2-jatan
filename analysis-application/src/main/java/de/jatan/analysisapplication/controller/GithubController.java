f79eed38cb8ed0dc5f6b3baa142c83c13c4169f3
Jan Vogt <jan_vogt92@web.de>
2020-06-05 09:02:53 +0200

    fix: remove unused import

--- analysis-application/src/main/java/de/jatan/analysisapplication/controller/GithubController.java
+++ analysis-application/src/main/java/de/jatan/analysisapplication/controller/GithubController.java
@@ -1,7 +1,6 @@
 package de.jatan.analysisapplication.controller;
 
 import java.util.List;
-import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
