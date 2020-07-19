1ea52ae15036bc1d8e0c215b955ce4778c939598
Jan Vogt <jan_vogt92@web.de>
2020-07-19 14:50:25 +0200

    refactor: rename Service and Model

---
+++ analysis-application/src/main/java/de/jatan/analysisapplication/Domain/Model/SonarQubeResults.java
@@ -0,0 +1,21 @@
+package de.jatan.analysisapplication.Domain.Model;
+
+public class SonarQubeResults {
+    private SonarResultsComponents component;
+
+    public SonarResultsComponents getComponent ()
+    {
+        return component;
+    }
+
+    public void setComponent (SonarResultsComponents component)
+    {
+        this.component = component;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [component = "+component+"]";
+    }
+}
