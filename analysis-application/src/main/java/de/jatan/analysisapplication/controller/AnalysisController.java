1a158cfb3bd10164ef03f5b6428b0b91cd6dc68c
Jan Vogt <jan_vogt92@web.de>
2020-08-12 23:28:56 +0200

    :recycle: remove unused imports

--- analysis-application/src/main/java/de/jatan/analysisapplication/controller/AnalysisController.java
+++ analysis-application/src/main/java/de/jatan/analysisapplication/controller/AnalysisController.java
@@ -1,8 +1,6 @@
 package de.jatan.analysisapplication.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
@@ -11,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
